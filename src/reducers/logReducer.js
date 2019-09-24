e31fb490ef60a42366238df9c100fc000b9b4763
Annan Wang <annanwang@Annans-MBP.attlocal.net>
2019-09-24 14:06:54 -0700

    add reducer

---
+++ src/reducers/logReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  logs: null,
+  current: null,
+  loading: false,
+  errir: null
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
